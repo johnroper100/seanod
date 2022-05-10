000d00bc475413f8caed92f8f471173ced5f7c7e
John Roper <johnroper100@gmail.com>
2022-05-10 15:15:09 -0400

    add mirage theme

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer class="probootstrap-footer" role="contentinfo">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="probootstrap-copyright">&copy; <?php echo date("Y"); ?> <?php echo $siteTitle; ?> - All Rights Reserved.</p>
+                <!--<ul class="probootstrap-social">
+                    <li><a href="#"><i class="icon-twitter"></i></a></li>
+                    <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                    <li><a href="#"><i class="icon-instagram2"></i></a></li>
+                    <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                </ul>-->
+                <ul class="probootstrap-main-nav" style="float: right;">
+                    <?php
+                        $menuItems = getMenuItems('header');
+                        foreach ($menuItems as $menuItem) {
+                    ?>
+                    <li><a style="color: #5068A9;" href="<?php echo BASEPATH . '/' . $menuItem['link']; ?>" <?php if ($menuItem['type'] == 1) { ?>target="_blank"<?php } ?>><?php echo $menuItem['name']; ?></a></li>
+                    <?php }; ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="gototop js-top">
+    <a href="#" class="js-gotop"><i class="icon-chevron-thin-up"></i></a>
+</div>
+
+
+<script src="<?php echo THEMEPATH; ?>/js/scripts.min.js"></script>
+<script src="<?php echo THEMEPATH; ?>/js/main.min.js"></script>
+<script src="<?php echo THEMEPATH; ?>/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,31 @@
+<header role="banner" class="probootstrap-header">
+    <div class="container">
+        <a href="<?php echo ORIGBASEPATH; ?>" class="probootstrap-logo"><?php echo $siteTitle; ?></a>
+        
+        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
+        <div class="mobile-menu-overlay"></div>
+
+        <nav role="navigation" class="probootstrap-nav hidden-xs">
+          <ul class="probootstrap-main-nav">
+            <?php
+                $menuItems = getMenuItems('header');
+                foreach ($menuItems as $menuItem) {
+            ?>
+            <li <?php if ($menuItem['type'] == 0 && $menuItem['page'] == $page['_id']) { ?>class="active"<?php }; ?>><a href="<?php echo BASEPATH . '/' . $menuItem['link']; ?>" <?php if ($menuItem['type'] == 1) { ?>target="_blank"<?php } ?>><?php echo $menuItem['name']; ?></a></li>
+            <?php }; ?>
+          </ul>
+          <!--<div class="extra-text visible-xs"> 
+            <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
+            <h5>Address</h5>
+            <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+            <h5>Connect</h5>
+            <ul class="social-buttons">
+              <li><a href="#"><i class="icon-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li><a href="#"><i class="icon-instagram2"></i></a></li>
+            </ul>
+          </div>-->
+        </nav>
+    </div>
+  </header>
+  <!-- END: header -->
